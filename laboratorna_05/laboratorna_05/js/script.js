d499969d44d44cb44cd4fd1492e66325d55c6eb0
artem2510 <129032150+artem2510@users.noreply.github.com>
2023-04-12 11:04:03 +0300

    Lab_05
    lab_05

---
+++ laboratorna_05/laboratorna_05/js/script.js
@@ -0,0 +1,7 @@
+
+const heading = document.querySelector('h1');
+heading.textContent = 'Нова лабораторна №5. Змінено JS';
+
+const list = document.querySelector('ul');
+list.style.color = 'green';
+
